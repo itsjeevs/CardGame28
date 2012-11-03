d067b75f984ee8f389369ed7e41efd051cb7e193
meriyast <meriya.st@gmail.com>
2012-11-02 20:49:13 -0500

    Committing Deck, Team, Card classes

--- CardGame28/src/cards/Deck.java
+++ CardGame28/src/cards/Deck.java
@@ -2,9 +2,7 @@ package cards;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
 
 public class Deck implements Iterator<Object> {
 	protected ArrayList<Card> myCardList;
@@ -44,75 +42,3 @@ public class Deck implements Iterator<Object> {
 
 
 
-
-
-class Card implements ICard{
-	
-	private final int mySuit;     
-	private final int myRank;
-	private final int myvalue;
-
-
-	private static final String[] suitStrings = {
-		"spades", "hearts", "diamonds", "clubs"	
-	};
-	
-	private String cardDesc;
-	
-	private static final String[] rankStrings = {
-		"jack","nine","ace","ten","king","queen","eight","seven"	
-	};
-
-	
-	
-	
-	public Card(int suit, int rank){
-		Map<String, Integer> valueMap = new HashMap<String, Integer>();
-		valueMap.put(rankStrings[0], 3);
-		valueMap.put(rankStrings[1], 2);
-		valueMap.put(rankStrings[2], 1);
-		valueMap.put(rankStrings[3], 1);
-		valueMap.put(rankStrings[4], 0);
-		valueMap.put(rankStrings[5], 0);
-		valueMap.put(rankStrings[6], 0);
-		valueMap.put(rankStrings[7], 0);
-		
-		mySuit = suit;
-		myRank = rank;
-		myvalue = valueMap.get((rank));
-		
-		cardDesc = rankStrings[getRank()]+" of "+
-	             suitStrings[getSuit()];
-	}
-
-	public int getMyvalue() {
-		return myvalue;
-	}
-	
-	public int getSuit() {
-		return mySuit;
-	}
-	
-	public int getRank() {
-		return myRank;
-	}
-	public String toString(){
-		return cardDesc;
-	}
-	
-	public int compareTo(Object o) {
-		ICard other = (ICard) o;
-		int rdiff = getRank() - other.getRank();
-		if (rdiff == 0) {
-			return getSuit() - other.getSuit();
-		}
-		else {
-			return rdiff;
-		}
-	}
-	
-	public boolean equals(Object o){
-	    return compareTo(o) == 0;
-	}
-	
-}
--- CardGame28/src/cards/Team.java
+++ CardGame28/src/cards/Team.java
@@ -9,6 +9,50 @@ public class Team {
 	private int totalPoints;
 	
 	private ArrayList<Player> players;
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	public int getTotalPoints() {
+		return totalPoints;
+	}
+
+	public void setTotalPoints(int totalPoints) {
+		this.totalPoints = totalPoints;
+	}
+
+	public ArrayList<Player> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(ArrayList<Player> players) {
+		this.players = players;
+	}
 	
+	
+	public Team addPlayer(Player p) {
+		
+		if(this.players.size() < 2) {
+			this.players.add(p);
+		} else {
+			//Need to throw an exception
+		}
+		return this;
+	}
+	
+	public int getTotalPoints(ArrayList<Card> ourCards) {
+		
+		int total =0;
+		for(Card c: ourCards) {
+			total += c.getMyvalue();
+		}
+		
+		return total;
+	}
 	
 }
